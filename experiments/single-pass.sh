82738cad344c3814bbcf76b6c877c4cc0bcca2ad
thaiminhpv <thaiminhpv@gmail.com>
2025-09-18 14:30:47 +0700

    add single pass random sampling

--- experiments/single-pass.sh
+++ experiments/single-pass.sh
@@ -1,12 +1,139 @@
 #!/bin/bash
 
-
+# Baseline Singlepass
 seq 0 163 | parallel --bar -j 48 '
 echo "Processing task ID: {}"
-python generate_and_test_postconditions.py \
+python generate_and_test_postconditions_singlepass.py \
     --task-id "HumanEval/{}" \
     --max-attempts 1 \
-    --output-dir "output/new-singlepass/run-with-completeness-threshold_90/max4/" \
+    --output-dir "output/baseline-singlepass/run-with-completeness-threshold_90/max4/" \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# Random Sampling Singlepass
+# t = 50, u = 4
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 4 \
+    --completeness-threshold 50 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_50/max4/ \
+    || {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 50, u = 8
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 8 \
+    --completeness-threshold 50 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_50/max8/ \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 50, u = 12
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 12 \
+    --completeness-threshold 50 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_50/max12/ \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 70, u = 4
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 4 \
+    --completeness-threshold 70 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_70/max4/ \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 70, u = 8
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 8 \
+    --completeness-threshold 70 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_70/max8/ \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 70, u = 12
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 12 \
+    --completeness-threshold 70 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_70/max12/ \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 90, u = 4
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 4 \
+    --completeness-threshold 90 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_90/max4/ \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 90, u = 8
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 8 \
+    --completeness-threshold 90 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_90/max8/ \
+|| {
+    echo "Error processing task ID: {}" >&2
+    exit 1
+}
+'
+# t = 90, u = 12
+seq 0 163 | parallel --bar -j 48 '
+echo "Processing task ID: {}"
+python generate_and_test_postconditions_general.py \
+    --mode singlepass \
+    --task-id HumanEval/{} \
+    --max-turns 12 \
+    --completeness-threshold 90 \
+    --output-dir output/random-sampling-singlepass/run-with-completeness-threshold_90/max12/ \
 || {
     echo "Error processing task ID: {}" >&2
     exit 1
